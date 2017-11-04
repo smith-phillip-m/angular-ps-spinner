831435c0eb225388324ad2e33e4b0cf2da37c995
smith-phillip-m <smith.phillip.m@gmail.com>
2017-11-03 20:44:58 -0400

    meh

--- src/app/loaders/cog/cog.component.ts
+++ src/app/loaders/cog/cog.component.ts
@@ -3,99 +3,100 @@ import { Component, Input, OnChanges } from '@angular/core';
 @Component({
   selector: 'app-cog',
   styles: [`
-    .container {
+    .gear-container {
       position: relative;
       z-index: 1000;
       border-radius: 50%;
+      margin: 0 auto;
     }
 
     .gear-face {
       position: absolute;
       z-index: 1002;
-      height: 60%;
-      width: 60%;
-      top: 20%;
-      left: 20%;
+      height: 76%;
+      width: 76%;
+      top: 12%;
+      left: 12%;
       border-radius: 50%;
     }
 
     .gear-center {
       position: absolute;
       z-index: 1003;
-      height: 35%;
-      width: 35%;
-      top: 32.5%;
-      left: 32.5%;
+      height: 40%;
+      width: 40%;
+      top: 30%;
+      left: 30%;
       border-radius: 50%;
     }
 
-    .tooth {
+    .gear-tooth {
       position: absolute;
       z-index: 1000;
-      height: 12.5%;
-      width: 12.5%;
+      height: 20%;
+      width: 20%;
       border-radius: 30% 0 0 30%;
     }
 
-    .tooth1 {
-      top: 43.25%;
-      left: 10%;
+    .gear-tooth1 {
+      top: 40%;
+      left: 2%;
     }
 
-    .tooth2 {
-      top: 21%;
-      left: 19%;
+    .gear-tooth2 {
+      top: 13%;
+      left: 13%;
       transform: rotate(45deg);
     }
 
-    .tooth3 {
-      top: 10%;
-      left: 43.25%;
+    .gear-tooth3 {
+      top: 2%;
+      left: 40%;
       transform: rotate(90deg);
     }
 
-    .tooth4 {
-      top: 21%;
-      right: 19%;
+    .gear-tooth4 {
+      top: 13%;
+      right: 13%;
       transform: rotate(135deg);
     }
 
-    .tooth5 {
-      top: 43.25%;
-      right: 10%;
+    .gear-tooth5 {
+      top: 40%;
+      right: 2%;
       transform: rotate(180deg);
     }
 
-    .tooth6 {
-      bottom: 21%;
-      right: 19%;
+    .gear-tooth6 {
+      bottom: 13%;
+      right: 13%;
       transform: rotate(225deg);
     }
 
-    .tooth7 {
-      bottom: 10%;
-      left: 43.25%;
+    .gear-tooth7 {
+      bottom: 2%;
+      left: 40%;
       transform: rotate(270deg);
     }
 
-    .tooth8 {
-      bottom: 21%;
-      left: 19%;
+    .gear-tooth8 {
+      bottom: 13%;
+      left: 13%;
       transform: rotate(315deg);
     }
   `],
   template: `
-    <div class="container animate-spin-slow" [ngStyle]="containerStyle">
+    <div class="gear-container animate-spin-slow" [ngStyle]="containerStyle">
       <div class="gear-face" [ngStyle]="gearFaceStyle"></div>
       <div class="gear-center" [ngStyle]="gearCenterStyle"></div>
-      <div class="tooth tooth1" [ngStyle]="toothStyle"></div>
-      <div class="tooth tooth2" [ngStyle]="toothStyle"></div>
-      <div class="tooth tooth3" [ngStyle]="toothStyle"></div>
-      <div class="tooth tooth4" [ngStyle]="toothStyle"></div>
-      <div class="tooth tooth5" [ngStyle]="toothStyle"></div>
-      <div class="tooth tooth6" [ngStyle]="toothStyle"></div>
-      <div class="tooth tooth7" [ngStyle]="toothStyle"></div>
-      <div class="tooth tooth8" [ngStyle]="toothStyle"></div>
+      <div class="gear-tooth gear-tooth1" [ngStyle]="toothStyle"></div>
+      <div class="gear-tooth gear-tooth2" [ngStyle]="toothStyle"></div>
+      <div class="gear-tooth gear-tooth3" [ngStyle]="toothStyle"></div>
+      <div class="gear-tooth gear-tooth4" [ngStyle]="toothStyle"></div>
+      <div class="gear-tooth gear-tooth5" [ngStyle]="toothStyle"></div>
+      <div class="gear-tooth gear-tooth6" [ngStyle]="toothStyle"></div>
+      <div class="gear-tooth gear-tooth7" [ngStyle]="toothStyle"></div>
+      <div class="gear-tooth gear-tooth8" [ngStyle]="toothStyle"></div>
     </div>
   `
 })
